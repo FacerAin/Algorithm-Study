13028a389fd3a8de235b23ff8c8de8b656d94cba
FacerAin <syw5141@daum.net>
2019-11-19 01:56:50 +0900

    Update 11-19

--- Baekjoon/Math/1267 핸드폰 요금/1267.cpp
+++ Baekjoon/Math/1267 핸드폰 요금/1267.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
     int T;
     cin >> T;
+    int sumy = 0;
+    int summ = 0;
     while (T--)
     {
-        }
+        double num;
+        cin >> num;
+        sumy += ceil((num+1) / 30) * 10;
+        summ += ceil((num+1) / 60) * 15;
+    }
+    if(sumy == summ){
+        cout << 'Y' <<' ' << 'M' << ' ' << sumy;
+    }else if(sumy > summ){
+        cout << 'M' << ' ' << summ;
+    }else{
+        cout << 'Y' << ' ' << sumy;
+    }
     return 0;
 }
